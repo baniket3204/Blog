c43f81adb6d84d264bf804ac738b88f7d9863f26
baniket3204 <baniket3204@gmail.com>
2023-02-20 12:43:49 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,17 @@
 
 function Home(){
+
+    const handleClick = () => {
+        console.log("hello world");
+    }
+
     return (
+
     <div className='home'>
     <h2>Home Page</h2>
-   </div>
-    
-    
-    );
+    <button onClick = {handleClick}>click me</button>
+   </div>   
+ );
 }
 
 export default Home;
