439b04768e0e4440819605a6ada99a8ba006a7e8
baniket3204 <baniket3204@gmail.com>
2023-03-03 23:09:54 +0530

    .

---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function notFound(){
+ return(
+    <div className="not-found">
+        <h2>Sorry</h2>
+        <p>That page cannot be found</p>
+        <Link to="/">Back to Homepage</Link>
+    </div>
+  
+ );
+}
+
+export default notFound;
